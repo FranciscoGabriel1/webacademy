01afb2125bc486685a7c13b98edc172f0eb9c91c
Francisco Gabriel <francisco@icomp.ufam.edu.br>
2023-09-30 09:23:33 -0400

    add script prod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT ?? 7777
+
 const dotenv = require("dotenv")
 dotenv.config()
+
+const PORT = process.env.PORT ?? 7777
 
 
 
